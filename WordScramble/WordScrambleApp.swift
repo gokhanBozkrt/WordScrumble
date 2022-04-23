2c3d61b81671ef3722016da517dee55dc8340807
gokhanBozkrt <99559446+gokhanBozkrt@users.noreply.github.com>
2022-04-23 21:44:09 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gokhan Bozkurt on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
